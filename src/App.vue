e5ce2aff12e4101e2c2e1d2814db2a8241f271a6
junwei.luo <1104049235>
2022-12-05 23:01:32 +0800

    demo提交

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <demo></demo>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import demo from "@/components/demo";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    demo
   }
 }
 </script>
